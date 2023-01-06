f534769b62ecac7fe099cd5bdf2b0541ee12de5d
hivkosic <117200270+hivkosic@users.noreply.github.com>
2023-01-07 00:48:59 +0100

    zad1SortiraniNiz & zad2Brackets done

--- zad1SortiraniNiz.js
+++ zad1SortiraniNiz.js
@@ -1,6 +1,7 @@
 /* 
 1. Koristeci dvije ugnjezdene for petlje napisi funkciju koja kao ulazni parametar prima niz brojeva,
-te kao rezultat vraca sortirani niz brojeva od veceg prema manjem. 
+te kao rezultat vraca sortirani niz brojeva od manjeg prema većem. 
+
 
 Primjer:
 Input: [4,2,6,4,2,1]
@@ -8,3 +9,45 @@ Output: [1,2,2,4,4,6]
 
 Dodatno: Pokusaj analizirati koliki bi bio ukupni broj iteracija za niz proizvoljne duzine.
 */
+
+
+//proba - bez funkcije 
+
+/*let arr = [4, 2, 6, 4, 2, 1];
+let change = [];
+
+for (let i = 0; i < 5; i++) {
+  for (let j = i + 1; j < 6; j++) {
+    if (arr[i] > arr[j]) {
+      change = arr[i];
+      arr[i] = arr[j];
+      arr[j] = change;
+    }
+  }
+}
+
+console.log(arr);
+console.log("broj iteracija:", numbIter);
+
+//Broj iteracija = (arr.length-1)*(arr.length - 1) ?????
+*/
+
+
+
+function sorted(arr) {
+  let change = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j <= arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        change = arr[i];
+        arr[i] = arr[j];
+        arr[j] = change;
+      }
+    }
+  }
+  return arr;
+}
+
+let numb = [4, 2, 6, 4, 2, 1];
+console.log(sorted(numb));
+
--- zad2Brackets.js
+++ zad2Brackets.js
@@ -15,3 +15,62 @@ Primjer 3:
 Input: '(c * (a+b)) * (x - (y * (c - d)))'
 Output: true
 */
+
+
+
+/*proba - bez funkcije 
+
+let br1=0;
+let br2 =0;
+const str = "(c * (a+b)) * (x - (y * (c - d)))"
+
+for (let i = 0; i<str.length; i++) {
+    if (str[i] === "(") {
+      br1 = br1 +1; 
+    }
+ else if (str[i] === ")") {
+      br2 = br2 +1;
+       };
+    }
+
+
+    if (br1+br2<1) {
+      console.log("ovaj niz stringova ne sadrži zagrade");
+    } else if (br1===br2) 
+{console.log ("true")} 
+    else {console.log("false");
+  }
+*/
+
+
+
+
+//br1 broji otvorene zagrade, a br2 zatvorene
+
+function brackets(str) {
+  let br1 = 0;
+  let br2 = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      br1 += 1;
+    } else if (str[i] === ")") {
+      br2 += 1;
+      }   
+  }
+  
+  if (br1+br2===0) {return "nema zagrada u stringu"};
+  return ((br1+br2>0)&&(br1===br2))? "true" : "false";
+}
+
+
+const prim1 = "(a+b) * (c - d)";
+const prim2 = "(a+b * (c - d)";
+const prim3 = "(c * (a+b)) * (x - (y * (c - d)))";
+const prim4 = "";
+
+
+console.log(brackets(prim1));
+console.log(brackets(prim2));
+console.log(brackets(prim4));
+console.log(brackets(prim3));
+
