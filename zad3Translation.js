5d2fad4c0afa531f2ec8843e5e3d0947dfb40499
hivkosic <117200270+hivkosic@users.noreply.github.com>
2023-01-07 19:44:18 +0100

    zad3 ver.1 done

--- zad3Translation.js
+++ zad3Translation.js
@@ -16,3 +16,44 @@ b) File ce kao ulazni parametar (process.argv) primati jezik. Ukoliko je jezik e
 zamjeniti sa engleskim recenicama. Ukoliko je jezik hr ili bilo koji drugi, funkcija ce tokene zamjeniti 
 sa prethodno navedenim hrvatskim recenicama.
 */
+
+
+function translate (key){
+    if (locale==="en") {
+        return english[key];
+    }
+    
+    if (locale==="hr") {
+        return croatian[key];
+    }
+
+    return croatian [key];
+}
+
+
+const croatian = {
+    "hello_world": "Pozdrav svima, mi smo studenti!",
+    "winter_assignment": "Za praznike smo dobili domaci.",
+    "first_week" : "Prvi tjedan smo odmarali.",
+    "second_week" : "Drugi tjedan smo radili.",
+    "success_result" : "Svi koji su uspjesno rijesili su odmarali i dalje.",
+    "fail_result": "Oni koji nisu uspjesno rjesili radili su vise."    
+};
+
+const english = {
+    "hello_world": "hello everyone, we are students!",
+    "winter_assignment": "we get a homework for vacation",
+    "first_week" : "we rested for the first week",
+    "second_week" : "the second week we worked",
+    "success_result" : "all who successfully solved rested",
+    "fail_result": "those who didn't, worked harder."    
+};
+
+
+const locale = process.argv[2] || "hr";
+console.log("pozdrav:", translate("hello_world"));
+console.log("zimski zadatak:", translate("winter_assignment"));
+console.log("prvi tjedan:", translate("first_week"));
+console.log("drugi tjedan:", translate("second_week"));
+console.log("dobar rezultat:", translate("success_result"));
+console.log("loš rezultat:", translate("fail_result"));
